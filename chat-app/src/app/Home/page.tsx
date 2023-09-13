14fd41fc017f883a54f4fbf84e6b40d0fc5bdc46
oisinegan <oisinegan00@gmail.com>
2023-09-13 15:02:44 +0100

    restart

--- chat-app/src/app/Home/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Login() {
-    return (
-        <main className="flex flex-col bg-red-500 items-center justify-center">Home Screen</main>
-    );
-  }
